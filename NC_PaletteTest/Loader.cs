e9f74be62b750acfb028bf3c61d8bc096d88f681
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2024-10-06 19:17:19 +0300

    upload project

---
+++ NC_PaletteTest/Loader.cs
@@ -0,0 +1,33 @@
+﻿using Teigha.Runtime;
+
+namespace NC_PaletteTest
+{
+    public class Loader : IExtensionApplication
+    {
+        private static HostMgd.Windows.PaletteSet? ps_Demo;
+
+        [CommandMethod("NC_SAMPLE_PALETTE_OPEN")]
+        public void PaletteLoad()
+        {
+            if (ps_Demo == null)
+            {
+                ps_Demo = new HostMgd.Windows.PaletteSet("WPF Test Palette", "WPF_UserControl_Sample", new Guid("CA51059C-5347-410B-9225-43E3155C6475"));
+                ps_Demo.MinimumSize = new System.Drawing.Size(200, 300);
+                //target_ps.Size = new System.Drawing.Size(target_info.Size[0], target_info.Size[1]);
+                ps_Demo.AddVisual("", new PaletteDemo());
+            }
+
+            ps_Demo.Visible = true;
+
+        }
+        public void Initialize()
+        {
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
